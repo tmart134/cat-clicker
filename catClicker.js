db00e45100f3256edc4b90a9ced649fc86f2b62c
Tracey Martin <martinltracey@gmail.com>
2018-10-04 17:26:30 -0400

    added second cat and cat names

--- catClicker.js
+++ catClicker.js
@@ -1,10 +1,28 @@
 //cat clicker js
-let clickCounter=0;
+let firstCat = "Buddy";
+let secondCat = "Heidi";
+let firstCatClickCounter=0;
+let secondCatClickCounter=0;
+let clickMessage;
+
+//display the cat names
+document.getElementById('first-cat-name').innerHTML = firstCat;
+document.getElementById('second-cat-name').innerHTML = secondCat;
 
 
-$('.cat-image').click(function(e) {
+$('.first-cat').click(function(e) {
 
-  	clickCounter++;
-  	document.getElementById("clicks").innerHTML = clickCounter;
+//update the click count and display the count and cat name
+  	firstCatClickCounter++;
+  	clickMessage = `${firstCat}! ${firstCatClickCounter}`;
+  	document.getElementById('first-cat-clicks').innerHTML = clickMessage;
 
+});
+
+$('.second-cat').click(function(e){
+
+//update the click count and display the count and cat name
+	secondCatClickCounter++;
+	clickMessage = `${secondCat}! ${secondCatClickCounter}`;
+	document.getElementById('second-cat-clicks').innerHTML = clickMessage;
 });
